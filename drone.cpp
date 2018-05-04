92cca4a5c984d581e74332f46bf9e39536929707
dufufeisdu <fufei.du@gmail.com>
2018-05-04 04:04:35 -0700

    (feat) Init Demo

---
+++ drone.cpp
@@ -0,0 +1,21 @@
+//
+//  drone.cpp
+//  OpenGlTutalExample
+//
+//  Created by FUFEI DU on 5/3/18.
+//  Copyright © 2018 FUFEI DU. All rights reserved.
+//
+
+#include "drone.hpp"
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <vector>
+class Drone{
+public:
+  Drone(){}
+private:
+  
+  
+};
---
+++ shader_m.c
@@ -0,0 +1,9 @@
+//
+//  shader_m.c
+//  OpenGlTutalExample
+//
+//  Created by FUFEI DU on 4/30/18.
+//  Copyright © 2018 FUFEI DU. All rights reserved.
+//
+
+#include "shader_m.h"
